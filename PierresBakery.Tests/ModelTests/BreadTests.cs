09ea4b098390b3119d7ef6d5edd5fe92154f9214
Billy Lee <codingbillylee@gmail.com>
2023-04-22 23:33:13 -0700

    Add test for calculating bread price

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -16,5 +16,13 @@ namespace BreadTests.Tests
       int ClassAmt = bread.Quantity;
       Assert.AreEqual(amt, ClassAmt);
     }
+
+    [TestMethod]
+    public void CalcPrice_ReturnPrice_String()
+    {
+      Bread bread = new Bread(9);
+      string Price = bread.CheckCost();
+      Assert.AreEqual("30", Price);
+    }
   }
 }
