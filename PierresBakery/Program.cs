fe63c4df1419a31c7ad1271e3ebb393e57f7aadd
Billy Lee <codingbillylee@gmail.com>
2023-04-22 21:24:36 -0700

    Add initial files

---
+++ PierresBakery/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace PierresBakery
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery");
+      Console.WriteLine("We sell bread for 5 dollars a loaf and we have a buy two get one deal.");
+      Console.WriteLine("We also sell pasteries for 2 dollars each and we have a buy three get one deal.");
+      Console.WriteLine("How many breads would you like to purchase?");
+      Console.WriteLine("How many pasteries would you like to purchase?");
+    }
+  }
+}
